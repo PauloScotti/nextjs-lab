471c9828f240eb9ed8f89f1e5f21b0ecd9adb392
PauloScotti <paulo.scotti@hotmail.com>
2022-03-02 22:30:14 -0300

    aprendendo o conceito

---
+++ components/Pessoa.js
@@ -0,0 +1,11 @@
+export default function Pessoa(props) {
+    return (
+        <div className="pessoa">
+            <b>Nome</b>: {props.nome}
+            <br/>
+            <b>Idade</b>: {props.idade}
+            <br/>
+            <br/>
+        </div>
+    );
+}
